d915a581094e9a7a86cc5364379b8decc6c66435
Michael Termotto <mjtermotto11@gmail.com>
2023-10-04 17:27:11 +0200

    Added factorial method

--- factorial.py
+++ factorial.py
@@ -1,3 +1,13 @@
-def factorial(num):
-	print("First Commit")
-	pass
+def factorial(value):
+    #edge case: factorial of 0 is 1
+    if value == 0:
+        return 1
+    
+    factorial_value = 1
+    while value > 0:
+        factorial_value *= value
+        value -= 1
+    return factorial_value
+
+value = int(input("Input number: "))
+print(factorial(value))
