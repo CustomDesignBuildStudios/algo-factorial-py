512b3a75abdd4d066722be4a1daa999bd6377409
Michael Termotto <mjtermotto11@gmail.com>
2023-10-04 17:35:39 +0200

    Added comments

--- factorial.py
+++ factorial.py
@@ -1,13 +1,21 @@
+
 def factorial(value):
-    #edge case: factorial of 0 is 1
-    if value == 0:
-        return 1
     
-    factorial_value = 1
-    while value > 0:
-        factorial_value *= value
-        value -= 1
-    return factorial_value
+	"""
+	Given a number returns the factorial of the number
+	Arguments:
+	value  - string - Number to get create the factorial from
+	"""    
+	#edge case: factorial of 0 is 1
+	if value == 0:
+		return 1
+
+	
+	factorial_value = 1
+	while value > 0:
+		factorial_value *= value
+		value -= 1
+	return factorial_value
 
 value = int(input("Input number: "))
 print(factorial(value))
